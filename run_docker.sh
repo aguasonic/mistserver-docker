b513ff7899e95ae473ac9c74326f6aaddc69c241
Mark Fischer at Home <mark.fischer@nissan-usa.com>
2022-11-01 20:45:52 -0700

    Cleanup.

--- run_docker.sh
+++ run_docker.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-# docker run -d --name mistserver --restart=always --net=host -v /home/mark/media:/media aguasonic_mist-server
-
-docker run -d --name ag_mistserver3 --restart=always --net=host -v /temp:/media aguasonic/mist-server
-
-
+docker run -d --name mistserver-docker --restart=always --net=host -v /temp:/media aguasonic/mist-server
 
